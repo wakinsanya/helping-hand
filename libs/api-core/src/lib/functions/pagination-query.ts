ce4162272660a9814f91cc92245bd4976f4d92b0
William Akinsanya <akinsanya.will@gmail.com>
2020-01-12 08:58:14 -0500

    generalize pagination as sub-aggregation

---
+++ libs/api-core/src/lib/functions/pagination-query.ts
@@ -0,0 +1,48 @@
+import { PaginationOptions } from '../interfaces/pagination-options.interface';
+
+export function paginationQuery(opts: PaginationOptions) {
+  const facetStage: any = {
+    $facet: {
+      [`${opts.entity}`]: [
+        {
+          $skip: opts.skip
+        },
+        {
+          $limit: opts.limit
+        }
+      ],
+      [`${opts.entity}TotalCount`]: [
+        {
+          $count: 'count'
+        }
+      ]
+    }
+  };
+
+  let sortStage: any;
+
+  if (opts.sort && (!opts.sortField || !opts.sortOrder)) {
+    throw new Error('Sorting requires a sort field and sort order');
+  } else if (opts.sort && opts.sortField && opts.sortOrder) {
+    sortStage = {
+      $sort: {
+        [`${opts.sortField}`]: opts.sortOrder === 'ascending' ? 1 : -1
+      }
+    };
+  }
+
+  const projectStage = {
+    $project: {
+      [`${opts.entity}`]: 1,
+      [`${opts.entity}TotalCount`]: {
+        $arrayElemAt: [`$${opts.entity}TotalCount`, 0]
+      }
+    }
+  };
+
+  if (sortStage) {
+    facetStage.$facet[`${opts.entity}`].unshift(sortStage);
+  }
+
+  return [facetStage, projectStage];
+}
---
+++ libs/api-core/src/lib/interfaces/pagination-options.interface.ts
@@ -0,0 +1,32 @@
+declare type sortOrder = 'ascending' | 'descending';
+
+/**
+ * Interface describing pagination options that can be set.
+ */
+export interface PaginationOptions {
+  /**
+   * Whether to sort retrived documents. If true, both the sort field
+   * and the ascending flag are required.
+   */
+  sort: boolean;
+  /**
+   * Specifies the document odering.
+   */
+  sortOrder?: sortOrder;
+  /**
+   * Field to sort by,
+   */
+  sortField?: string;
+  /**
+   * Number of documents to be skipped.
+   */
+  skip: number;
+  /**
+   * Maximum number of documents to return.
+   */
+  limit: number;
+  /**
+   * Name of entity being paginated.
+   */
+  entity: string;
+}
