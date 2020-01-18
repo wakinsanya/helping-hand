9fd582af3ae03a579cb05cb91b8e782ce52c5de6
William Akinsanya <akinsanya.will@gmail.com>
2020-01-18 15:54:56 +0000

    update notificaiton controller

--- apps/api/src/app/notification/controllers/notification.controller.ts
+++ apps/api/src/app/notification/controllers/notification.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, Query } from '@nestjs/common';
 import { NotificationService } from '@api/notification/services/notification.service';
+import { SubscriptionLabel } from '@helping-hand/api-common';
+import { Observable } from 'rxjs';
 
 @Controller('notifications')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post()
-  create(@Query('label') label: string) {
-
+  create(@Query('label') label: string): Observable<boolean> {
+    return this.notificationService.create(label as SubscriptionLabel);
   }
 }
