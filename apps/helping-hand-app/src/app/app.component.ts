5d676ee1a9dcb20112739963b409b085ad126221
William Akinsanya <akinsanya.will@gmail.com>
2020-03-24 21:53:37 +0000

    fix router animations

--- apps/helping-hand-app/src/app/app.component.ts
+++ apps/helping-hand-app/src/app/app.component.ts
@@ -1,21 +1,54 @@
 import { Component } from '@angular/core';
-import { slide, transform, fade, step } from './route-animations';
 import { RouterOutlet } from '@angular/router';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'helping-hand-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [fade]
+  animations: [
+    trigger('fade', [
+      transition('* <=> *', [
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              left: 0,
+              width: '100%',
+              opacity: 0,
+              transform: 'scale(0) translateY(100%)'
+            })
+          ],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [
+            animate(
+              '300ms ease',
+              style({ opacity: 1, transform: 'scale(1) translateY(0)' })
+            )
+          ],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class AppComponent {
+  animation: string;
   constructor() {}
 
   prepareRoute(outlet: RouterOutlet) {
-    return (
-      outlet &&
-      outlet.activatedRouteData &&
-      outlet.activatedRouteData['animation']
-    );
+    return  outlet &&
+    outlet.activatedRouteData &&
+    outlet.activatedRouteData['state'];
   }
 }
